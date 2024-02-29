0fd9dfb83aad4b670a8a74205f3768d157925c27
yasa22 <spartyasaga22@gmail.com>
2024-02-29 09:29:09 +0100

    añadidas secciones

--- detalles.php
+++ detalles.php
@@ -20,6 +20,8 @@
                             tambi&eacute;n ofrecemos la posibilidad de alquilar instrumentos y plataformas de reproducci&oacute;n musical, permiti&eacute;ndote explorar 
                             y experimentar con diferentes g&eacute;neros y estilos.
                         </p>
+                    </section>
+                    <section>
                         <h2>Guia de usuarios</h2>
                         <p>
                             Para nuestros usuarios registrados, ofrecemos una experiencia a&uacute;n m&aacute;s personalizada. 
@@ -27,18 +29,23 @@
                             As&iacute;, cada vez que compras o vendes en nuestra plataforma, estás invirtiendo en tu pr&oacute;xima gran aventura musical. 
                             Pero no te preocupes si no est&aacute;s registrado. Tambi&eacute;n ofrecemos una selecci&oacute;n de productos para nuestros usuarios no registrados. 
                             As&iacute;, todos pueden disfrutar de la m&uacute;sica, sin importar su nivel de compromiso.
-                        </p>      
+                        </p>
+                    </section>
+                    <section>      
                         <h2>&iquest;Que puedes hacer en nuestra web?</h2>
                         <p>
                             Nuestro objetivo es proporcionar una plataforma eficiente y f&aacute;cil de usar que satisfaga todas tus necesidades de comercio electr&oacute;nico. 
                             Aqu&iacute; te presentamos algunas de las funcionalidades clave que ofrecemos:
                         </p>
+                    </section>
+                    <section>
                             <ol> 
                                 <li><em><strong>Gesti&oacute;n</strong></em> de stock de art&iacute;culos de la tienda online.</li>
                                 <li><em><strong>Compra</strong></em> de los art&iacute;culos, guardando los pedidos de los usuarios registrados en un carrito y un listado de pedidos realizados.</li>
                                 <li><em><strong>Venta</strong></em> de art&iacute;culos a la tienda para que esta los pueda adquirir para su posible futura venta. Que se comporte como un chat entre dos usuarios.</li>
                                 <li><em><strong>Sistema de valoraciones y comentarios</strong></em> por parte de los compradores de cierto producto para que los futuros compradores sean informados.</li>
                             </ol>
+                    </section>
                         <p>
                             En resumen, nuestra plataforma es m&aacute;s que una simple tienda online. Es un espacio donde la m&uacute;sica cobra vida, donde puedes explorar, descubrir y experimentar la m&uacute;sica de una manera completamente nueva.
                         </p>
