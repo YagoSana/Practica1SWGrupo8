9961f12e34cd4037fbca9732d4a764cc8c983180
Farissi <melfaris@ucm.es>
2024-03-05 21:15:15 +0100

    Añadir una seccion "Añadir producto" si se loguea como admin

--- lateralIzq.php
+++ lateralIzq.php
@@ -12,6 +12,11 @@
         <li><a href="../miembros.php">Miembros</a></li>
         <li><a href="../bocetos.php">Bocetos</a></li>
         <li><a href="../planificacion.php">Planificación</a></li>
+        <?php
+        if(isset($_SESSION["esAdmin"])) {
+            echo '<li><a href="../uploadArticulo.php">Añadir productos</a></li>';
+        }
+        ?>
         </ul>
     </nav>
 </html>
