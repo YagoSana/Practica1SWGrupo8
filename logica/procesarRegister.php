70305e8c4a1537de05934931d87e5c2435f7b00d
Laura Reyes <laurreye@ucm.es>
2024-03-07 10:33:18 +0100

    cambios bd register

--- logica/procesarRegister.php
+++ logica/procesarRegister.php
@@ -1,18 +1,18 @@
 <?php
-    $db = new mysqli('127.0.0.1', 'root', '', 'registro');
+    $db = new mysqli('127.0.0.1', 'username', 'password', 'bd_def');
 
     if($db->connect_errno) {
         echo "Error al conectarse con la base de datos: " . $db -> connect_error;
         exit();
     }
 
-    $nombre = $db -> real_escape_string($_POST['nombre']);
-    $apellido = $db -> real_escape_string($_POST['apellido']);
-    $email = $db -> real_escape_string($_POST['email']);
-    $usuario = $db -> real_escape_string($_POST['usuario']);
-    $contraseña = $db -> real_escape_string($_POST['contraseña']);
+    $Nombre = $db -> real_escape_string($_POST['Nombre']);
+    $Apellido = $db -> real_escape_string($_POST['Apellido']);
+    $Email = $db -> real_escape_string($_POST['Email']);
+    $User = $db -> real_escape_string($_POST['User']);
+    $Pass = $db -> real_escape_string($_POST['Pass']);
 
-    $sql = "INSERT INTO articulos (articulo_id, articulo_nombre, articulo_descripcion, articulo_precio) VALUES ('$articulo_id', '$articulo_nombre', '$articulo_descripcion', '$articulo_precio')";
+    $sql = "INSERT INTO usuario (Nombre, Apellido, Email, User, Pass) VALUES ('$Nombre', '$Apellido', '$Email', '$User', '$Pass')";
 
     if (!$db -> query($sql)) {
         printf("%d Row inserted.\n", $db->affected_rows);
--- vistas/register.php
+++ vistas/register.php
@@ -14,17 +14,17 @@
                 <h2>Regístrate en BackMusic</h2>
                 <form action="../logica/procesarRegister.php" method="POST">
                 <p>
-                    <input type="text" id="usuario" name="usuario" placeholder="Nombre de usuario" size="30" required>
+                    <input type="text" id="User" name="User" placeholder="Nombre de usuario" size="30" required>
                 </p>
                 <p>
-                    <input type="text" id="nombre" name="nombre" placeholder="Nombre" size="12" required>
-                    <input type="text" id="apellido" name="apellido" placeholder="Apellido" size="12" required>
+                    <input type="text" id="Nombre" name="Nombre" placeholder="Nombre" size="12" required>
+                    <input type="text" id="Apellido" name="Apellido" placeholder="Apellido" size="12" required>
                 </p>
                 <p>
-                    <input type="Email" id="email" name="email" placeholder="email" size="30" required>
+                    <input type="Email" id="Email" name="Email" placeholder="Email" size="30" required>
                 </p>
                 <p>
-                    <input type="password" id="contraseña" name="contraseña" placeholder="Contraseña" size="30" required>
+                    <input type="password" id="Pass" name="Pass" placeholder="Contraseña" size="30" required>
                 </p>
                 <p>
                     <input type="checkbox" id="terminos" name="terminos" required>
