9ba660e40a49e7db97d02a9a477f1509eb86dac6
yasa22 <spartyasaga22@gmail.com>
2024-03-09 11:45:30 +0100

    añdido config.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    
+    require_once 'logica/config.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
---
+++ logica/config.php
@@ -0,0 +1,26 @@
+<?php
+/* */
+/* Parámetros de configuración de la aplicación */
+/* */
+
+// Parámetros de configuración generales
+define('RUTA_APP', '/Practica2'); //cada uno pone aqui el nombre del directorio donde tiene la web en localhost
+define('RUTA_IMGS', RUTA_APP . '/img');
+define('RUTA_CSS', RUTA_APP . '/estilos');
+
+// Parámetros de configuración de la BD
+define('BD_HOST', 'localhost');
+define('BD_NAME', 'bd_def');
+define('BD_USER', 'bd_def');
+define('BD_PASS', 'bd_def');
+
+
+/* */
+/* Configuración de Codificación y timezone */
+/* */
+
+ini_set('default_charset', 'UTF-8');
+setLocale(LC_ALL, 'es_ES.UTF.8');
+date_default_timezone_set('Europe/Madrid');
+
+//no es necesario cerrar los php
