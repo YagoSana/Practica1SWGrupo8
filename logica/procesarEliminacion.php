a0bc90166a3283454a6b4adbf6496b52e65939ab
Laura Reyes <laurreye@ucm.es>
2024-03-10 14:37:35 +0100

    modificacion eliminar

--- logica/procesarEliminacion.php
+++ logica/procesarEliminacion.php
@@ -14,7 +14,7 @@
         // Usa la instancia de Database que ya creaste en baseDatos.php
         $connection = $db->getConnection();
 
-        $producto = new Producto(null, null, null, null, $db->getConnection());
+        $producto = new Producto(null, null, null, null, null, $db->getConnection());
         $producto->deleteProducto($ID);
 
     }
