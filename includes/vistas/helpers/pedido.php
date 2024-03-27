fb848bba96751e18a9afbd040f30a30683236fe8
Alvaro Felix <alfelix@ucm.es>
2024-03-27 19:50:26 +0100

    Cambios en la clase pedido

--- includes/vistas/helpers/pedido.php
+++ includes/vistas/helpers/pedido.php
@@ -6,10 +6,13 @@ class Pedido {
     private $Cliente;
     private $Producto;
     private $Cantidad;
+    private $Estado;
 
     public function __construct($usuario) {
         $this->usuario = $usuario;
         $this->productos = [];
+        $this->Fecha = date('Y-m-d', strtotime('+2 days'));
+        $this->Estado = 'Pendiente';
     }
 
     public function agregarProducto($productoId) {
@@ -22,6 +25,22 @@ class Pedido {
 
         // En este ejemplo, simplemente devolvemos los productos agregados al pedido
         return $this->productos;
+    }
+
+
+    public function entregarPedido() {
+        if ($this->fechaEntrega <= date('Y-m-d')) {
+            $this->estado = 'entregado';
+            foreach ($this->productos as $producto) {
+                echo "<button onclick='valorar(\"{$producto->getID()}\")'>Valorar</button>";
+            }
+        } else {
+            echo "Aún no has recibido tu pedido.";
+        }
+    }
+
+    public function getEstado() {
+        return $this->Estado;
     }
 }
 
