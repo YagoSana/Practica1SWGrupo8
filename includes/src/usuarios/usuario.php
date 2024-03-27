fe7ec3fefa6da7fd7034363aae52d1a3b82f2cfd
yasa22 <spartyasaga22@gmail.com>
2024-03-27 17:41:14 +0100

    cambio en usuario (habia 2 clases en el mismo php)

--- includes/src/usuarios/usuario.php
+++ includes/src/usuarios/usuario.php
@@ -1,5 +1,5 @@
 <?php
-    include("carrito.php");
+include ("carrito.php");
 class Usuario
 {
 
@@ -15,129 +15,7 @@ class Usuario
         }
         return false;
     }
-    
-    public static function crea($nombreUsuario, $password, $nombre, $rol)
-    {
-        $user = new Usuario($nombreUsuario, self::hashPassword($password), $nombre);
-        $user->añadeRol($rol);
-        return $user->guarda();
-    }
 
-    public static function buscaUsuario($nombreUsuario)
-    {
-        $conn = Aplicacion::getInstance()->getConexionBd();
-        $query = sprintf("SELECT * FROM Usuarios U WHERE U.nombreUsuario='%s'", $conn->quote($nombreUsuario));
-        $rs = $conn->query($query);
-        $result = false;
-        if ($rs) {
-            $fila = $rs->fetch(PDO::FETCH_ASSOC);
-            if ($fila) {
-                $result = new Usuario($fila['nombreUsuario'], $fila['password'], $fila['nombre'], $fila['id']);
-            }
-        } else {
-            error_log("Error BD ({$conn->errorCode()}): {$conn->errorInfo()}");
-        }
-        return $result;
-    }
-
-    public static function buscaPorId($idUsuario)
-    {
-        $conn = Aplicacion::getInstance()->getConexionBd();
-        $query = sprintf("SELECT * FROM Usuarios WHERE id=%d", $idUsuario);
-        $rs = $conn->query($query);
-        $result = false;
-        if ($rs) {
-            $fila = $rs->fetch(PDO::FETCH_ASSOC);
-            if ($fila) {
-                $result = new Usuario($fila['nombreUsuario'], $fila['password'], $fila['nombre'], $fila['id']);
-            }
-        } else {
-            error_log("Error BD ({$conn->errorCode()}): {$conn->errorInfo()}");
-        }
-        return $result;
-    }
-    
-    private static function hashPassword($password)
-    {
-        return password_hash($password, PASSWORD_DEFAULT);
-    }
-
-    private static function cargaRoles($usuario)
-    {
-        $roles=[];
-            
-        $conn = Aplicacion::getInstance()->getConexionBd();
-        $query = sprintf("SELECT RU.rol FROM RolesUsuario RU WHERE RU.usuario=%d"
-            , $usuario->id
-        );
-        $rs = $conn->query($query);
-        if ($rs) {
-            $roles = $rs->fetchAll(PDO::FETCH_ASSOC);
-
-            $usuario->roles = [];
-            foreach($roles as $rol) {
-                $usuario->roles[] = $rol['rol'];
-            }
-            return $usuario;
-
-        } else {
-            error_log("Error BD ({$conn->errorCode()}): {$conn->errorInfo()}");
-        }
-        return false;
-    }
-   
-    private static function inserta($usuario)
-    {
-        $result = false;
-        $conn = Aplicacion::getInstance()->getConexionBd();
-        $query=sprintf("INSERT INTO Usuarios(nombreUsuario, nombre, password) VALUES ('%s', '%s', '%s')"
-            , $conn->quote($usuario->nombreUsuario)
-            , $conn->quote($usuario->nombre)
-            , $conn->quote($usuario->password)
-        );
-        if ( $conn->exec($query) ) {
-            $usuario->id = $conn->lastInsertId();
-            $result = self::insertaRoles($usuario);
-        } else {
-            error_log("Error BD ({$conn->errorCode()}): {$conn->errorInfo()}");
-        }
-        return $result;
-    }
-   
-    private static function insertaRoles($usuario)
-    {
-        $conn = Aplicacion::getInstance()->getConexionBd();
-        foreach($usuario->roles as $rol) {
-            $query = sprintf("INSERT INTO RolesUsuario(usuario, rol) VALUES (%d, %d)"
-                , $usuario->id
-                , $rol
-            );
-            if ( ! $conn->exec($query) ) {
-                error_log("Error BD ({$conn->errorCode()}): {$conn->errorInfo()}");
-                return false;
-            }
-        }
-        return $usuario;
-    }
-    
-    <?php
-    include("carrito.php");
-class Usuario
-{
-
-    public const ADMIN_ROLE = 1;
-
-    public const USER_ROLE = 2;
-
-    public static function login($nombreUsuario, $password)
-    {
-        $usuario = self::buscaUsuario($nombreUsuario);
-        if ($usuario && $usuario->compruebaPassword($password)) {
-            return self::cargaRoles($usuario);
-        }
-        return false;
-    }
-    
     public static function crea($nombreUsuario, $password, $nombre, $rol)
     {
         $user = new Usuario($nombreUsuario, self::hashPassword($password), $nombre);
@@ -178,7 +56,7 @@ class Usuario
         }
         return $result;
     }
-    
+
     private static function hashPassword($password)
     {
         return password_hash($password, PASSWORD_DEFAULT);
@@ -186,18 +64,20 @@ class Usuario
 
     private static function cargaRoles($usuario)
     {
-        $roles=[];
-            
+        $roles = [];
+
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $query = sprintf("SELECT RU.rol FROM RolesUsuario RU WHERE RU.usuario=%d"
-            , $usuario->id
+        $query = sprintf(
+            "SELECT RU.rol FROM RolesUsuario RU WHERE RU.usuario=%d"
+            ,
+            $usuario->id
         );
         $rs = $conn->query($query);
         if ($rs) {
             $roles = $rs->fetchAll(PDO::FETCH_ASSOC);
 
             $usuario->roles = [];
-            foreach($roles as $rol) {
+            foreach ($roles as $rol) {
                 $usuario->roles[] = $rol['rol'];
             }
             return $usuario;
@@ -207,18 +87,23 @@ class Usuario
         }
         return false;
     }
-   
+
     private static function actualiza($usuario)
     {
         $result = false;
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $query=sprintf("UPDATE Usuarios U SET nombreUsuario = %s, nombre=%s, password=%s WHERE U.id=%d"
-            , $conn->quote($usuario->nombreUsuario)
-            , $conn->quote($usuario->nombre)
-            , $conn->quote($usuario->password)
-            , $usuario->id
+        $query = sprintf(
+            "UPDATE Usuarios U SET nombreUsuario = %s, nombre=%s, password=%s WHERE U.id=%d"
+            ,
+            $conn->quote($usuario->nombreUsuario)
+            ,
+            $conn->quote($usuario->nombre)
+            ,
+            $conn->quote($usuario->password)
+            ,
+            $usuario->id
         );
-        if ( $conn->exec($query) ) {
+        if ($conn->exec($query)) {
             $result = self::borraRoles($usuario);
             if ($result) {
                 $result = self::insertaRoles($usuario);
@@ -226,41 +111,45 @@ class Usuario
         } else {
             error_log("Error BD ({$conn->errorCode()}): {$conn->errorInfo()}");
         }
-        
+
         return $result;
     }
-   
+
     private static function borraRoles($usuario)
     {
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $query = sprintf("DELETE FROM RolesUsuario RU WHERE RU.usuario = %d"
-            , $usuario->id
+        $query = sprintf(
+            "DELETE FROM RolesUsuario RU WHERE RU.usuario = %d"
+            ,
+            $usuario->id
         );
-        if ( ! $conn->exec($query) ) {
+        if (!$conn->exec($query)) {
             error_log("Error BD ({$conn->errorCode()}): {$conn->errorInfo()}");
             return false;
         }
         return $usuario;
     }
-    
+
     private static function borra($usuario)
     {
         return self::borraPorId($usuario->id);
     }
-    
+
     private static function borraPorId($idUsuario)
     {
         if (!$idUsuario) {
             return false;
-        } 
+        }
         /* Los roles se borran en cascada por la FK
          * $result = self::borraRoles($usuario) !== false;
          */
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $query = sprintf("DELETE FROM Usuarios U WHERE U.id = %d"
-            , $idUsuario
+        $query = sprintf(
+            "DELETE FROM Usuarios U WHERE U.id = %d"
+            ,
+            $idUsuario
         );
-        if ( ! $conn->exec($query) ) {
+        if (!$conn->exec($query)) {
             error_log("Error BD ({$conn->errorCode()}): {$conn->errorInfo()}");
             return false;
         }
@@ -284,7 +173,7 @@ class Usuario
     private $roles;
 
     private function __construct($nombreUsuario, $password, $nombre, $apellido, $email, $id = null, $roles = [])
-{
+    {
         $this->$id = $id;
         $this->$nombreUsuario = $nombreUsuario;
         $this->$password = $password;
@@ -294,7 +183,7 @@ class Usuario
         $this->$roles = $roles;
 
         $this->carrito = new Carrito();
-}
+    }
 
     public function getId()
     {
@@ -353,7 +242,7 @@ class Usuario
     {
         $this->password = self::hashPassword($nuevoPassword);
     }
-    
+
     public function guarda()
     {
         if ($this->id !== null) {
@@ -361,7 +250,7 @@ class Usuario
         }
         return self::inserta($this);
     }
-    
+
     public function borrate()
     {
         if ($this->id !== null) {
