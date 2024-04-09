cb0935dcb20f777f5402c2ce5c5c9f5fe1741479
Alvaro Felix <alfelix@ucm.es>
2024-04-09 18:40:13 +0200

    Creacion para procesa la eliminacion de productos del carrito

---
+++ includes/vistas/helpers/procesarEliminacionCarrito.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../plantillas/compras.php';
+require_once '../../config.php';
+require_once 'baseDatos.php';
+require_once 'producto.php';
+
+$db = new Database(BD_HOST, BD_USER, BD_PASS, BD_NAME);
+$db->connect();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $producto_id = $_POST['productoId']; // Recupera el ID del producto enviado desde el cliente
+
+    // Accede al usuario y a su carrito desde la sesión
+    $carrito = $_SESSION['usuario']->getCarrito();
+
+    // Elimina el producto del carrito
+    $carrito->eliminarProducto($producto_id);
+
+    echo "Producto eliminado del carrito";
+}
