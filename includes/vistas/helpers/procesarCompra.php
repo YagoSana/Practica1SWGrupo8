7eb49ed21bd14a26c91f5579cf092ae09e508783
yasa22 <spartyasaga22@gmail.com>
2024-04-09 12:53:15 +0200

    cambio en rutas y requires

--- includes/vistas/helpers/procesarCompra.php
+++ includes/vistas/helpers/procesarCompra.php
@@ -1,8 +1,8 @@
 <?php
 
 require_once '../../config.php';
-require RAIZ_APP. '/includes/src/usuarios/usuario.php';
-require RAIZ_APP. '/includes/vistas/helpers/carrito.php';
+require_once RAIZ_APP. '/includes/src/usuarios/usuario.php';
+require_once RAIZ_APP. '/includes/vistas/helpers/carrito.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['login'])) {
     // Obtiene el carrito de compras del usuario
@@ -24,6 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['login'])) {
 } else {
     // Si el usuario no ha iniciado sesión o si la solicitud no es POST,
     // redirige al usuario a la página de inicio de sesión.
-    header("Location: login.php");
+    header("Location: ". RUTA_APP ."/includes/src/login.php");
     exit;
 }
