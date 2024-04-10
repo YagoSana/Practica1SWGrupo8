c5c866b9714f4adc16ddd636ba9f67db7cfe1205
yagosana <yagosana@ucm.es>
2024-04-10 19:26:27 +0200

    cambio en texto

--- includes/vistas/plantillas/principal.php
+++ includes/vistas/plantillas/principal.php
@@ -19,16 +19,14 @@ require_once __DIR__ . '/../../config.php';
                 <section>
                     <h2>¿Quiénes somos?</h2>
                     <p>
-                        Somos una pequeña tienda en el centro de Madrid, en el barrio Chamartín, que quiere hacer el
-                        acceso a la
-                        música algo para todos. Se puede disfrutar de nuestros servicios tanto online como en la tienda,
-                        desde comprar instrumentos hasta poder venderlos y recibir dinero para próximos descuentos en la
-                        tienda.
+                        Back Music es una pequeña tienda en el centro de Madrid, localizada en el barrio Chamartín, que quiere hacer el acceso a la música algo para todos. 
+                        Se puede disfrutar de nuestros servicios tanto online como en la tienda, desde comprar instrumentos hasta poder vender los tuyos propios y 
+                        recibir dinero para próximos descuentos en la tienda.
                     </p>
-                </section>
-                <section>
-                    <h2>Artículos destacados</h2>
-                    <p>Aquí se mostrarán los artículos más vendidos de la tienda.</p>
+                    <p>
+                        Nuestro equipo está formado por un grupo de jóvenes apasionados por la música y la tecnología, que quieren hacer de la compra de instrumentos 
+                        algo sencillo y accesible para todos.
+                    </p>
                 </section>
             </article>
         </main>
