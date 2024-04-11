67a8febce21219208898d7916dde179049239b9a
Alvaro Felix <alfelix@ucm.es>
2024-04-11 10:44:50 +0200

    Se han resuelto los errores con las valoraciones

--- includes/vistas/helpers/procesarValoracion.php
+++ includes/vistas/helpers/procesarValoracion.php
@@ -8,11 +8,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $producto_id = $_POST['producto_id']; // Recupera el ID del producto enviado desde el cliente
     $valoracion = $_POST['valoracion']; // Recupera la valoración enviada desde el cliente
     $comentario = $_POST['comentario']; // Recupera el comentario enviado desde el cliente
-
-    // Aquí asumimos que ya tienes una función para buscar un producto por su ID
-    $connection = $db->getConnection();
-
-    // Aquí asumimos que ya tienes una función para establecer una valoración
     usuario::valorarProducto($producto_id, $_SESSION['usuario']->getId(), $valoracion, $comentario);
 }
 
