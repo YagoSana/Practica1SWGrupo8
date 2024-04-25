717993713724d707b7f1afc9663e79bf304fc173
Farissi <melfaris@ucm.es>
2024-04-25 19:52:59 +0200

    new

--- includes/vistas/helpers/producto.php
+++ includes/vistas/helpers/producto.php
@@ -189,11 +189,6 @@ class Producto
         return $this->Nombre;
     }
 
-    public function getStock()
-    {
-        return $this->Stock;
-    }
-
     public function getID()
     {
         return $this->ID;
