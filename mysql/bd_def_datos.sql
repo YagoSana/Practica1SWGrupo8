12ae90bd1fe1f4c57f22793414ea63cdeb537168
yagosana <yagosana@ucm.es>
2024-05-02 12:29:35 +0200

    cambio en datos.sql

--- mysql/bd_def_datos.sql
+++ mysql/bd_def_datos.sql
@@ -3,10 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-04-2024 a las 11:15:13
+-- Tiempo de generación: 02-05-2024 a las 12:29:04
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bd_def_datos`
+-- Base de datos: `bd_def`
 --
 
 --
@@ -26,29 +27,30 @@ SET time_zone = "+00:00";
 --
 
 INSERT INTO `carrito` (`ID_Carrito`, `Cliente`, `Producto`, `Cantidad`) VALUES
-(2, '8', 8, 11);
+(3, '11', 5, 1),
+(4, '11', 6, 1);
 
 --
 -- Volcado de datos para la tabla `pedidos`
 --
 
 INSERT INTO `pedidos` (`ID_Pedido`, `Fecha`, `Cliente`, `Importe`) VALUES
-(1, '2024-04-09', '8', 299.99),
-(2, '2024-04-10', '9', 299.99),
-(3, '2024-04-29', '8', 44);
+(1, '2024-05-02', '8', 0),
+(2, '2024-05-02', '9', 0);
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
 INSERT INTO `productos` (`ID_Producto`, `Nombre`, `Descripcion`, `Precio`, `Imagen`, `Stock`, `Visible`, `Tipo`, `ID_Venta`) VALUES
-(1, 'Guitarra Acústica', 'Instrumento musical de alta calidad, con cuerdas de acero y un sonido resonante y claro. Ideal para músicos de todos los niveles. Cuerpo de madera pulida para una estética elegante. Esta guitarra acús', 299.99, '/img/imagenesBD/Guitarra.jpg', 10, 1, 'Cuerda', 0),
+(1, 'Guitarra Acústica', 'Instrumento musical de alta calidad, con cuerdas de acero y un sonido resonante y claro. Ideal para músicos de todos los niveles. Cuerpo de madera pulida para una estética elegante. Esta guitarra acús', 299.99, '/img/imagenesBD/Guitarra.jpg', 7, 1, 'Cuerda', 0),
 (2, 'Tambor', 'Instrumento de percusión de sonido profundo y resonante. Cuerpo de madera con parches de piel ajustables. Perfecto para ritmos enérgicos y actuaciones en vivo.', 99.99, '/img/imagenesBD/Tambor.jpg', 10, 1, 'Percusion', 0),
 (3, 'Arpa', 'Instrumento musical elegante con cuerdas tensadas que se tocan con los dedos. Produce sonidos melodiosos y suaves. Ideal para música clásica y celta. Cuerpo de madera de alta calidad para una resonanc', 629.99, '/img/imagenesBD/Arpa.jpg', 10, 1, 'Cuerda', 0),
 (4, 'Armónica', 'diseñada para practicantes, principiantes e intermedios. Gran sistema de armónica adecuado para blues, folk, música clásica pop, jazz, country y rock & roll', 68.20, '/img/imagenesBD/armonica.jpg', 10, 1, 'Viento', 0),
 (5, 'Pink Floyd Vinilo', 'Vinilo de la banda Pink Floyd, con su álbum más famoso, The Dark Side of the Moon. Un clásico de la música rock que no puede faltar en tu colección.', 35.99, '/img/imagenesBD/PinkFloydVinile.png', 10, 1, 'Articulos', 0),
 (6, 'PumpUpTheJam Vinilo', 'Vinilo de la banda Technotronic, con su álbum más famoso, Pump Up The Jam. Un clásico de la música electrónica que conquistó las pistas de baile en los años 90.', 35.99, '/img/imagenesBD/PumpUpTheJam.png', 10, 1, 'Articulos', 0),
-(7, 'Camiseta Los Ramones', 'Camiseta de la banda Ramones, con su icónico logo en la parte delantera. Fabricada en algodón 100% para una mayor comodidad y durabilidad. Disponible unicamente en talla L. (Más tallas próximamente)', 12.99, '/img/imagenesBD/Ramones.png', 10, 1, 'Articulos', 0);
+(7, 'Camiseta Los Ramones', 'Camiseta de la banda Ramones, con su icónico logo en la parte delantera. Fabricada en algodón 100% para una mayor comodidad y durabilidad. Disponible unicamente en talla L. (Más tallas próximamente)', 12.99, '/img/imagenesBD/Ramones.png', 10, 1, 'Articulos', 0),
+(8, 'Piano Eléctrico', 'Vendo un piano eléctrico poco usado, queria tener una obcion barata de música clasica en mi sótano pero ya no lo uso. Ha pillado algo de polvo pero no ha perdido valor alguino.', 85.00, '/img/imagenesBD/pianoElectrico.jpg', 1, 1, 'cuerda', 12);
 
 --
 -- Volcado de datos para la tabla `productos_pedidos`
@@ -56,8 +58,7 @@ INSERT INTO `productos` (`ID_Producto`, `Nombre`, `Descripcion`, `Precio`, `Imag
 
 INSERT INTO `productos_pedidos` (`ID_Pedido`, `ID_Producto`, `Cantidad`) VALUES
 (1, 1, 1),
-(2, 1, 1),
-(3, 8, 1);
+(2, 1, 2);
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -76,8 +77,16 @@ INSERT INTO `usuario` (`Apellido`, `Nombre`, `User`, `Idusuario`, `Pass`, `Email
 --
 
 INSERT INTO `valoraciones` (`ID_Valoracion`, `Idusuario`, `ID`, `Valoracion`, `Comentario`) VALUES
-(1, 8, 1, 5, 'Excelente guitarra. El sonido es claro y resonante, y el cuerpo de madera pulida es absolutamente hermoso.'),
-(2, 9, 2, 4, 'Buena guitarra para su precio. Las cuerdas de acero producen un sonido brillante y la guitarra en sí es bastante duradera.');
+(1, 8, 1, 5, 'Excelente guitarra para adentrarse en el mundo de la música, la madera se siente de calidad y las cuerdas de metal son fantásticas.'),
+(2, 9, 2, 4, 'Son buenas las guitarras que se venden en esta tienda. He comprado 2, una para mi y otra para mi hermano para tocar juntos y disfrutamos mucho de la calidad del producto. Como aspecto negativo podría decir que pesan un poco de más.');
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`ID_Venta`, `ID_Usuario`, `Nombre`, `Descripcion`, `Imagen`, `Estado`) VALUES
+(1, 12, 'Piano Eléctrico', 'Vendo un piano eléctrico poco usado, queria tener una obcion barata de música clasica en mi sótano pero ya no lo uso. Ha pillado algo de polvo pero no ha perdido valor alguino.', 'pianoElectrico.jpg', 'Aceptada');
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
