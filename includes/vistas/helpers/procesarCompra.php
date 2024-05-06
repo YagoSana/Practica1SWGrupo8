163f77f4576a57dcbc00f6f633bf7ea8eb00444f
yagosana <yagosana@ucm.es>
2024-05-06 13:45:25 +0200

    a ver si no la cago

--- includes/vistas/helpers/procesarCompra.php
+++ includes/vistas/helpers/procesarCompra.php
@@ -10,19 +10,16 @@ if (session_status() === PHP_SESSION_NONE) {
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['login'])) {
     $carrito = $_SESSION['usuario']->getCarrito();
     $total = $_POST['total'];
+    $totalSinDescuento = $_POST['totalSinDescuento']; // Aquí obtienes el total original
     $usarPuntos = isset($_POST['usarPuntos']);
     $puntos = Usuario::getPuntos($_SESSION['usuario']->getId());
 
     if ($usarPuntos) {
-        if ($total > $puntos) {
-            // Caso 1: El total es mayor que los puntos del wallet
-            // Resta los puntos del wallet al total y elimina todos los puntos del wallet
+        if ($total > 0) {
             Usuario::quitarWalletPoints($puntos, $_SESSION['usuario']->getId());
         } else {
-            // Caso 2: El total es menor o igual que los puntos del wallet
-            // Resta el total de los puntos del wallet y establece el total a cero
             
-            Usuario::quitarWalletPoints($total, $_SESSION['usuario']->getId());
+            Usuario::quitarWalletPoints($totalSinDescuento, $_SESSION['usuario']->getId());
         }
     }
 
