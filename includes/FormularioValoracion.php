0d4614cb683b74a51dbd57368c49c317f4d41288
Laura Reyes <laurreye@ucm.es>
2024-05-09 04:42:08 +0200

    mostrarvalo

---
+++ includes/FormularioValoracion.php
@@ -0,0 +1,72 @@
+<?php
+
+//require_once '../../config.php';
+//require_once '../../src/usuarios/usuario.php';
+//require_once '../../includes/src/usuarios/valoracion.php';
+require_once 'Formulario.php';
+
+class FormularioValoracion extends Formulario
+{
+    private $pedidoId;
+    private $productoId;
+
+    public function __construct($pedidoId, $productoId)
+    {
+        parent::__construct('FormValoracion', ['urlRedireccion' => RUTA_APP . '/includes/vistas/plantillas/mostrarValoracion.php?id=' . $pedidoId, 'method'=>'POST', 'enctype'=>'multipart/form-data']);
+        $this->pedidoId = $pedidoId;
+        $this->productoId = $productoId;
+    }
+
+    protected function generaCamposFormulario(&$datos)
+    {
+        $pedido_id = $datos['pedido_id'] ?? '';
+        $producto_id = $datos['producto_id'] ?? '';
+        $valoracion = $datos['valoracion'] ?? '';
+        $comentario = $datos['comentario'] ?? '';
+        
+        $erroresCampos = self::generaErroresCampos(['pedido_id', 'producto_id', 'valoracion', 'comentario'], $this->errores, 'span', array('class' => 'error'));
+
+        $htmlCamposFormulario = <<<EOS
+        <div class='val'>
+            <input type="hidden" id="pedido_id" name="pedido_id" value="$this->pedidoId">
+            <input type="hidden" id="producto_id" name="producto_id" value="$this->productoId ?>">
+            <label class="opcion">
+                <input type="radio" id="valoracion" name="valoracion" value="1"> 1
+            </label>
+            <label class="opcion">
+                <input type="radio" id="valoracion" name="valoracion" value="2"> 2
+            </label>
+            <label class="opcion">
+                <input type="radio" id="valoracion" name="valoracion" value="3"> 3
+            </label>
+            <label class="opcion">
+                <input type="radio" id="valoracion" name="valoracion" value="4"> 4
+            </label>
+            <label class="opcion">
+                <input type="radio" id="valoracion" name="valoracion" value="5"> 5
+            </label>
+            <textarea id="comentario" name="comentario" minlength="50" maxlength="1500" required></textarea>
+            <input type="submit" value="Enviar valoración">
+        </div>
+        EOS;
+
+        return $htmlCamposFormulario;
+    }
+
+    protected function procesaFormulario(&$datos)
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $pedidoId = $datos['pedido_id'];
+        $productoId = $datos['producto_id'];
+        $valoracion = $datos['valoracion'];
+        $comentario = $datos['comentario'];
+
+        // Realiza la valoración del producto
+        Usuario::valorarProducto($productoId, $pedidoId, $_SESSION['usuario']->getId(), $valoracion, $comentario);
+    }
+}
+
+?>
--- includes/vistas/plantillas/mostrarValoracion.php
+++ includes/vistas/plantillas/mostrarValoracion.php
@@ -1,67 +1,38 @@
 <?php
-require '../../config.php';
-require_once RAIZ_APP. '/includes/src/usuarios/usuario.php';
-require_once '../helpers/pedido.php';
-require_once '../helpers/producto.php';
+    require '../../config.php';
+    require_once RAIZ_APP. '/includes/src/usuarios/usuario.php';
+    require_once '../helpers/pedido.php';
+    require_once '../helpers/producto.php';
+    require_once '../../FormularioValoracion.php';
+    session_start();
+
+    $htmlFormVal = "";
+    $id_pedido = $_GET['id'];
+    $pedido = new Pedido($_SESSION['usuario']);
+    $productos = Pedido::obtenerProductosDelPedido($id_pedido);
+
+    foreach($productos as $producto) {
+        $prod = Producto::getProducto($producto['ID_Producto']);
+        $htmlFormVal .= "<div class='nom'>" . $prod->getNombre() . "</div>";
+
+        if($pedido->yaValorado($id_pedido, $producto['ID_Producto'])) {
+
+            $form = new FormularioValoracion($id_pedido, $producto['ID_Producto']);       
+            $htmlFormVal .= $form->gestiona();
+        
+        }else {
+
+            $htmlFormVal .= "<div class='val'><p>¡Ya has valorado este producto!</p></div>";
+        }
+    }
+
+    $botonVolver = "<button class='volver-btn' onclick=\"window.location.href='" . RUTA_APP . "/includes/vistas/plantillas/mostrarPedidos.php'\">Volver</button>";
+
+    $contenido = <<<EOS
+    $htmlFormVal
+    $botonVolver
+    EOS;
+
+
+    require_once RAIZ_APP . '/includes/vistas/plantillas/plantilla.php';
 ?>
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <?php include RAIZ_APP . '/includes/vistas/comun/header.php'; ?>
-    <title>Sistema de Valoracion BACK MUSIC</title>
-</head>
-
-<body>
-    <div id="contenedor">
-        <?php include RAIZ_APP . '/includes/vistas/comun/cabecera.php'; ?>
-        <?php include RAIZ_APP . '/includes/vistas/comun/lateralIzq.php'; ?>
-        <main>
-            <h2>Valore nuestros productos</h2>
-            <?php
-                $id_pedido = $_GET['id'];
-                $pedido = new Pedido($_SESSION['usuario']);
-                $productos = Pedido::obtenerProductosDelPedido($id_pedido);
-                foreach($productos as $producto) {
-                    $prod = Producto::getProducto($producto['ID_Producto']);
-                    echo $prod->getNombre();
-                    if($pedido->yaValorado($id_pedido, $producto['ID_Producto'])) {
-            ?>
-            <div class='pedido'>
-            <form action="<?php echo RUTA_APP; ?>/includes/vistas/helpers/procesarValoracion.php" method="POST">
-                <input type="hidden" name="pedido_id" value="<?php echo $id_pedido ?>">
-                <input type="hidden" name="producto_id" value="<?php echo $producto['ID_Producto'] ?>">
-                <label>
-                    <input type="radio" name="valoracion" value="1"> 1
-                </label>
-                <label>
-                    <input type="radio" name="valoracion" value="2"> 2
-                </label>
-                <label>
-                    <input type="radio" name="valoracion" value="3"> 3
-                </label>
-                <label>
-                    <input type="radio" name="valoracion" value="4"> 4
-                </label>
-                <label>
-                    <input type="radio" name="valoracion" value="5"> 5
-                </label>
-                <textarea name="comentario" minlength="50" maxlength="1500" required></textarea>
-                <input type="submit" value="Enviar valoración">
-            </form>
-                </div>
-            <?php
-                    }else {
-                        echo "<div class='pedido'>";
-                        echo "<p class='frase'>Ya has valorado este producto.</p>";
-                        echo "</div>";
-                    }
-                }
-            ?>
-            <button class='valorar-btn' onclick="window.location.href='<?php echo RUTA_APP; ?>/includes/vistas/plantillas/mostrarPedidos.php'">Volver</button>
-        </main>
-        <?php include RAIZ_APP . '/includes/vistas/comun/pieDePagina.php'; ?>
-    </div>
-</body>
-
-</html>
